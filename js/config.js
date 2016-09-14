0c1163733884ac35015b4cb0b9c6860a14d5caff
dmontanez <montanezd@leidos.com>
2016-09-13 20:28:20 -0700

    Pushed refactored code to main folder.

--- js/config.js
+++ js/config.js
@@ -1,79 +1,103 @@
-var cycleEnum = {
-    FORWARD: {
-        name: 'FORWARD',
-        value: 'forward'
-    },
-    BACKWARD: {
-        name: 'BACKWARD',
-        value: 'backward'
-    },
-    RANDOM: {
-        name: 'RANDOM',
-        value: 'random'
+/**
+ * Config Object
+ * @param {Object} userState
+ */
+function Config( userState ) {
+    
+    var self = this;
+    
+    this.cycleEnum = {
+        FORWARD: {
+            name: 'FORWARD',
+            value: 'forward'
+        },
+        BACKWARD: {
+            name: 'BACKWARD',
+            value: 'backward'
+        },
+        RANDOM: {
+            name: 'RANDOM',
+            value: 'random'
+        }
+    };
+    this.stateEnum = {
+        FIRSTLOAD: {
+            name: 'First Load',
+            value: 'firstLoad'
+        },
+        INITIAL: {
+            name: 'Initial',
+            value: 'initial'
+        },
+        QUIZZING: {
+            name: 'Quizzing',
+            value: 'quizzing'
+        },
+        FINISHED: {
+            name: 'Finished',
+            value: 'finished'
+        }
+    };
+    this.quizEnum = {
+        TEST: {
+            name: 'TEST',
+            value: 'test'
+        },
+        STANDARD: {
+            name: 'STANDARD',
+            value: 'standard'
+        },
+        REVIEW: {
+            name: 'REVIEW',
+            value: 'review'
+        }
+    };
+    
+    this.appState              = undefined;
+    this.cycle                 = undefined;
+    this.deckLimit             = undefined;
+    this.endMessageFront       = 'Congratulations!';
+    this.endMessageBack        = 'You did it!';
+    this.flashDuration         = 750;
+    this.flipDuration          = 2000;
+    this.flipOnHover           = undefined;
+    this.masteryLevel          = 3;
+    this.persistState          = undefined;
+    this.quizType              = undefined;
+    this.showReponseCount      = undefined;
+    this.showReponseIndicators = undefined;
+    this.showTimer             = undefined;
+    this.version               = '1.1.0';
+    
+    this.default = {
+        appState                : self.stateEnum.FIRSTLOAD,
+        cycle                   : self.cycleEnum.FORWARD,   
+        quizType                : self.quizEnum.REVIEW,
+        deckLimit               : Number.MAX_VALUE,
+        flipOnHover             : false,
+        persistState            : false,
+        showReponseCount        : true,
+        showReponseIndicators   : true,
+        showTimer               : true
+    };
+    this.state = userState || this.default;
+    
+    this.applyState(this.state);
+    
+}
+Config.prototype.setConfig = function( key, value ) {
+    this[key] = value;
+};
+Config.prototype.saveState = function() {
+    for(setting in this.state) {
+        this.state[setting] = this[setting];
     }
 };
-
-var stateEnum = {
-    FIRSTLOAD: {
-        name: 'First Load',
-        value: 'firstLoad'
-    },
-    INITIAL: {
-        name: 'Initial',
-        value: 'initial'
-    },
-    QUIZING: {
-        name: 'Quizzing',
-        value: 'quizzing'
-    },
-    FINISHED: {
-        name: 'Finished',
-        value: 'finished'
+Config.prototype.getState = function() {
+    return this.state;
+};
+Config.prototype.applyState = function( state ) {
+    for(setting in state) {
+        this.setConfig( setting, state[setting] );
     }
-};
-
-var quizEnum = {
-    TEST: {
-        name: 'TEST',
-        value: 'test'
-    },
-    STANDARD: {
-        name: 'STANDARD',
-        value: 'standard'
-    },
-    REVIEW: {
-        name: 'REVIEW',
-        value: 'review'
-    }
-};
-
-var config = {
-    appState                : stateEnum.FIRSTLOAD,
-    cycle                   : cycleEnum.FORWARD,
-    deckLimit               : Number.MAX_VALUE,
-    endMessageFront         : 'Congratulations!',
-    endMessageBack          : 'You did it!',
-    flashDuration           : 750,
-    flipDuration            : 2000,
-    flipOnHover             : undefined,
-    fromSavedState          : undefined,
-    masteryLevel            : 3,
-    quizType                : quizEnum.REVIEW,
-    saveConfig              : true,
-    saveDeck                : true,
-    showReponseCount        : undefined,
-    showReponseIndicators   : undefined,
-    showTimer               : undefined,
-    timerDuration           : 0,
-    timerEnd                : 0,
-    timerStart              : 0,
-    timerIncrement          : 100,
-    version                 : '1.0.0'
-};
-
-config.default = {
-    flipOnHover             : false,
-    showReponseCount        : true,
-    showReponseIndicators   : true,
-    showTimer               : true
 };
